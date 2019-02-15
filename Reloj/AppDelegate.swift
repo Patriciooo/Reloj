43bd9bd38b9aaf373aee8ee1b1646efbe9fcb3af
Patricio Alberola Gutiérrez <patricioalberola@outlook.com>
2019-02-15 19:14:45 +0100

    Initial Commit

---
+++ Reloj/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Reloj
+//
+//  Created by Patricio Alberola Gutiérrez on 15/02/2019.
+//  Copyright © 2019 Patricio Alberola Gutiérrez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
